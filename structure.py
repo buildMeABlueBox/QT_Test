a98fa46de3a6ac9e2ae7b315487c255c7c2daad5
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-05 21:01:54 -0400

    add grid layout

--- structure.py
+++ structure.py
@@ -3,23 +3,23 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 
-class HelloWorld(QDialog):
+class HelloWorld(QDialog): #QDialog is a Window
     def __init__(self):
         QDialog.__init__(self)
-        layout = QVBoxLayout()
+        layout = QGridLayout()
         label = QLabel("Hello World!")
         line_edit = QLineEdit()
         button = QPushButton("Close")
 
-        layout.addWidget(label)
-        layout.addWidget(line_edit)
-        layout.addWidget(button)
+        layout.addWidget(label, 0, 0)
+        layout.addWidget(line_edit, 0, 1)
+        layout.addWidget(button, 1, 1)
 
         self.setLayout(layout)
         button.clicked.connect(self.close)
 
 app = QApplication(sys.argv) #Only should have one instance of QApplication
-dialog = HelloWorld() #QDialog is a Window
+dialog = HelloWorld()
 dialog.show()
 app.exec_()
 
