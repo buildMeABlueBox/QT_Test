39346fb1a0fd6847ee2a40c9e7ffccf099285fa6
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-05 20:17:16 -0400

    import PyQt5 instead of 4. more imports

--- structure.py
+++ structure.py
@@ -1,4 +1,10 @@
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import sys
+
+app = QApplication(sys.argv) #Only should have one instance of QApplication
+dialog = QDialog() #QDialog is a Window
+dialog.show()
+app.exec_()
 
