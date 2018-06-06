4b1742ea7c099e5b2f0d8f6bf0585ea20c854dd1
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-05 20:29:55 -0400

    Hello World Dialog

--- structure.py
+++ structure.py
@@ -3,8 +3,22 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 
+class HelloWorld(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        layout = QVBoxLayout()
+        label = QLabel("Hello World!")
+        line_edit = QLineEdit()
+        button = QPushButton("Close")
+
+        layout.addWidget(label)
+        layout.addWidget(line_edit)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
 app = QApplication(sys.argv) #Only should have one instance of QApplication
-dialog = QDialog() #QDialog is a Window
+dialog = HelloWorld() #QDialog is a Window
 dialog.show()
 app.exec_()
 
