2dd1599a793d21a2c42d0ebf8c59b5e48dc9fc04
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-06-05 20:50:59 -0400

    close button event added to close the window

--- structure.py
+++ structure.py
@@ -16,6 +16,7 @@ class HelloWorld(QDialog):
         layout.addWidget(button)
 
         self.setLayout(layout)
+        button.clicked.connect(self.close)
 
 app = QApplication(sys.argv) #Only should have one instance of QApplication
 dialog = HelloWorld() #QDialog is a Window
